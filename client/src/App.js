e3b8f63ed697db9e94550ec30a1339719233365d
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-15 18:28:53 +0300

    done adding rails to

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  const [movies, setMovies]= useState([])
   useEffect(()=>{
-    fetch()
-  })
+    fetch('/movies')
+    .then(r=> r.json())
+    .then(movies=> setMovies(movies))
+  },[])
+  console.log(movies)
   return (
-   <>
-   </>
+    <>
+      <h1> Hello from React</h1>
+      <h2>Movie title list</h2>
+      {movies.map(movie=> <li>{movie.title}</li>)}
+    </>
   );
 }
 
